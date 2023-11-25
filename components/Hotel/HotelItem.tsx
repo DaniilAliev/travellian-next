df2876a691cd1f97f8a3b72084e6e6a4c8482df3
DaniilAliev <de.aliev@yandex.ru>
2023-11-25 14:33:26 +0300

    fixed fetching

--- components/Hotel/HotelItem.tsx
+++ components/Hotel/HotelItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import styles from './HotelItem.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import _ from "lodash";
@@ -9,6 +9,8 @@ import nextButton from '../../public/chevron-up.svg';
 import { useSelector } from "react-redux";
 import { selectOrder } from "@/slices/orderSlice";
 import getPrice from "../Destinations/getPrice";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 type Hotel = {
   adress: string,
@@ -22,7 +24,29 @@ type Hotel = {
   rating: number,
 };
 
-const HotelItem: FC<{hotel: Hotel}> = ({hotel}) => {
+const HotelItem = () => {
+  const [hotel, setHotel] = useState<Hotel>();
+  const [idPage, setId] = useState();
+
+  const router = useRouter();
+
+  const pathname = router.pathname;
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (id) {
+      const fetchData = async () => {
+        const res = await axios.get(`https://x8ki-letl-twmt.n7.xano.io/api:KAEwqeq2/destinations/${id}`);
+        console.log(res.data)
+        setHotel(res.data)
+      }
+  
+      fetchData()
+    }
+   
+
+  }, [idPage, router.query, id])
+
   const swiperNextRef: any = useRef();
   const swiperPrevRef: any = useRef();
 
@@ -31,27 +55,27 @@ const HotelItem: FC<{hotel: Hotel}> = ({hotel}) => {
   const guests = orderState.guestsNumber;
   const daysDiff = orderState.daysDiff;
 
-  const buttons = 
-  <div className={styles.buttons}>
-    <button className={styles.prevBtn} onClick={() => swiperPrevRef.current.slidePrev()}><Image src={prevButton} alt="prev" /></button>
-    <button className={styles.nextBtn} onClick={() => swiperNextRef.current.slideNext()}><Image src={nextButton} alt="next" /></button>
-  </div>;
-  
-  return (
+  const buttons =
+    <div className={styles.buttons}>
+      <button className={styles.prevBtn} onClick={() => swiperPrevRef.current.slidePrev()}><Image src={prevButton} alt="prev" /></button>
+      <button className={styles.nextBtn} onClick={() => swiperNextRef.current.slideNext()}><Image src={nextButton} alt="next" /></button>
+    </div>;
+
+  return ( hotel && 
     <section>
       <div className={styles['hotel-container']}>
         <div className={styles.info}>
           <div>
-            <h1>{hotel.name}</h1>
-            <p>{hotel.adress}</p>
+            <h1>{hotel?.name}</h1>
+            <p>{hotel?.adress}</p>
           </div>
           <div className={styles['buttons-and-score']}>
-            <div className={styles.score}><p>{hotel.rating}</p></div>
+            <div className={styles.score}><p>{hotel?.rating}</p></div>
             <button><p>Book now!</p></button>
           </div>
-          
+
         </div>
-    
+
         <Swiper
           className={styles.swiper}
           autoHeight
@@ -60,21 +84,21 @@ const HotelItem: FC<{hotel: Hotel}> = ({hotel}) => {
             swiperPrevRef.current = swiper;
           }}
         >
-          {hotel.pictures.map(picture => 
-              <SwiperSlide key={_.uniqueId()} className={styles.slide}>
-                <Image src={picture} width={10000} height={10000} alt="Hotel imge"/>
-              </SwiperSlide>
-            )}
+          {hotel?.pictures.map(picture =>
+            <SwiperSlide key={_.uniqueId()} className={styles.slide}>
+              <Image src={picture} width={10000} height={10000} alt="Hotel imge" />
+            </SwiperSlide>
+          )}
         </Swiper>
         <div className={styles['buttons-container']}>
           {buttons}
         </div>
-        <p className={styles.description}>{hotel.description}</p>
+        <p className={styles.description}>{hotel?.description}</p>
         <div className={styles['price-container']}>
-            
-            <p><span>{`Your price: €${getPrice(guests, hotel.price, daysDiff)}`}</span></p>
-            <p className={styles.nights}>{`${daysDiff} nights, ${orderState.guestsNumber} adults`}</p>
-          </div>
+
+          <p><span>{`Your price: €${getPrice(guests, hotel.price, daysDiff)}`}</span></p>
+          <p className={styles.nights}>{`${daysDiff} nights, ${orderState.guestsNumber} adults`}</p>
+        </div>
       </div>
     </section>
   )
--- pages/destinations/[id].tsx
+++ pages/destinations/[id].tsx
@@ -1,49 +1,7 @@
 import HotelItem from "@/components/Hotel/HotelItem";
-import axios from "axios";
-import { FC } from "react";
 
-type Hotel = {
-  adress: string,
-  city: string,
-  created_at: number,
-  description: string,
-  id: number,
-  name: string,
-  pictures: string[],
-  price: number,
-  rating: number,
-};
-
-export const getStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: true,
-  };
-};
-
-export const getStaticProps = async (context: any) => {
-	try {
-		const id = context.params.id;
-
-		const res = await axios.get(`https://x8ki-letl-twmt.n7.xano.io/api:KAEwqeq2/destinations/${id}`);
-		const hotel = res.data || null;
-
-		return {
-			props: {
-				hotel
-			},
-			revalidate: 60, // Обновить данные каждые 60 секунд
-		}
-	} catch(e) {
-		console.log(e)
-	}
-}
-
-const Hotel: FC<{hotel: Hotel}> = ({hotel}) => {
-	if (hotel) {
-		return <HotelItem hotel={hotel} />
-	};
-  return;
+const Hotel = () => {
+  return <HotelItem />
 }
 
 export default Hotel
