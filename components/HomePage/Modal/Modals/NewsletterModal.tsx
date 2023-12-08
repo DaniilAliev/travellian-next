ae3eeff9e2660d2d22589e4d069a1a574360aa4e
DaniilAliev <de.aliev@yandex.ru>
2023-12-08 16:05:34 +0300

    fix

--- components/HomePage/Modal/Modals/NewsletterModal.tsx
+++ components/HomePage/Modal/Modals/NewsletterModal.tsx
@@ -2,7 +2,7 @@ const NewsletterModal = () => {
   return (
 		<div>
 			<h1>Newsletter</h1>
-			<p>Thanks for the subscription! Check your email's inbox!</p>
+			<p>Thanks for the subscription! Check your inbox!</p>
 		</div>
 	)
 }
